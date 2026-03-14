eb5fa3a228214f62a6f97516a101a357bd1e9681
Lay Clough <lay.clough@gmail.com>
2026-03-14 14:10:59 -0700

    fix: set build target to esnext for top-level await

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   build: {
+    target: 'esnext',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
